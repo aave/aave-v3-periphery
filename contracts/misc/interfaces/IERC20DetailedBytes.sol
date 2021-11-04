55a2be961aca772a5762ad4e9b627271aae7de47
sendra <polsendra@gmail.com>
2021-11-04 11:36:44 +0100

    feat: updated with maker case

---
+++ contracts/misc/interfaces/IERC20DetailedBytes.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: agpl-3.0
+pragma solidity 0.8.7;
+
+import {IERC20} from '@aave/core-v3/contracts/dependencies/openzeppelin/contracts/IERC20.sol';
+
+interface IERC20DetailedBytes is IERC20 {
+  function name() external view returns (bytes32);
+
+  function symbol() external view returns (bytes32);
+
+  function decimals() external view returns (uint8);
+}

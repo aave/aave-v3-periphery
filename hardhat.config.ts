30a818f5ec574da83e174087a2c930fc4833b624
kartojal <canillas.mail@gmail.com>
2021-11-12 18:07:12 +0100

    feat: bump solc to 0.8.10 as @aave/core-v3

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -68,7 +68,7 @@ const mainnetFork = MAINNET_FORK
 // export hardhat config
 const config: HardhatUserConfig = {
   solidity: {
-    version: '0.8.7',
+    version: '0.8.10',
     settings: {
       optimizer: { enabled: true, runs: 25000 },
       evmVersion: 'berlin',

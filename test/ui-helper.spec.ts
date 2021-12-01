82be4cdabad4a6f42ebaa743edba0b2fdbbe2923
kartojal <canillas.mail@gmail.com>
2021-12-01 09:56:08 +0100

    tests: add the rest of calls for UiIncentiveDataProviderV3 at basic tests

--- test/ui-helper.spec.ts
+++ test/ui-helper.spec.ts
@@ -1,6 +1,8 @@
+import { advanceBlock } from '@aave/deploy-v3';
 import { UiIncentiveDataProviderV3 } from './../types/UiIncentiveDataProviderV3';
 import hre from 'hardhat';
 import { makeSuite } from './helpers/make-suite';
+import { expect } from 'chai';
 
 makeSuite('UI Incentives Helper', (testEnv) => {
   let uiHelper: UiIncentiveDataProviderV3;
@@ -11,11 +13,53 @@ makeSuite('UI Incentives Helper', (testEnv) => {
     uiHelper = await hre.ethers.getContractAt('UiIncentiveDataProviderV3', artifact.address);
   });
 
-  it('Test getter', async () => {
+  it('Call "getReservesIncentivesData"', async () => {
     const { addressesProvider } = testEnv;
     const response = await uiHelper.getReservesIncentivesData(addressesProvider.address, {
       gasLimit: '12000000',
     });
-    console.log('RES', response);
+    console.log('debug', response);
+    expect('getReservesIncentivesData').to.be.calledOnContractWith(uiHelper, [
+      addressesProvider.address,
+    ]);
+  });
+
+  it('Call "getFullReservesIncentiveData"', async () => {
+    const {
+      addressesProvider,
+      users: [user1],
+    } = testEnv;
+    const response = await uiHelper.getFullReservesIncentiveData(
+      addressesProvider.address,
+      user1.address,
+      {
+        gasLimit: '12000000',
+      }
+    );
+    console.log('debug', response);
+    expect('getFullReservesIncentiveData').to.be.calledOnContractWith(uiHelper, [
+      addressesProvider.address,
+      user1.address,
+    ]);
+  });
+
+  it('Call "getUserReservesIncentivesData"', async () => {
+    const {
+      addressesProvider,
+
+      users: [user1],
+    } = testEnv;
+    const response = await uiHelper.getUserReservesIncentivesData(
+      addressesProvider.address,
+      user1.address,
+      {
+        gasLimit: '12000000',
+      }
+    );
+    console.log('debug', response);
+    expect('getUserReservesIncentivesData').to.be.calledOnContractWith(uiHelper, [
+      addressesProvider.address,
+      user1.address,
+    ]);
   });
 });

b7951bb4de7f20116c3f823c34945774c3f8727f
miguelmtzinf <miguelmtz.mail@gmail.com>
2022-02-08 18:13:16 +0100

    fix: Remove unnecessary import in IRewardsDistributor

--- contracts/rewards/interfaces/IRewardsDistributor.sol
+++ contracts/rewards/interfaces/IRewardsDistributor.sol
@@ -1,8 +1,6 @@
 // SPDX-License-Identifier: agpl-3.0
 pragma solidity 0.8.10;
 
-import {RewardsDistributorTypes} from '../libraries/RewardsDistributorTypes.sol';
-
 interface IRewardsDistributor {
   event AssetConfigUpdated(
     address indexed asset,

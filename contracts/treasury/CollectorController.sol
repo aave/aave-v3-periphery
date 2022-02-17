56a39c2daf502ffe3da36072ffdfd8e73d8b47b9
kartojal <canillas.mail@gmail.com>
2022-02-17 18:26:32 +0100

    feat: add Collector implementation and controller

---
+++ contracts/treasury/CollectorController.sol
@@ -0,0 +1,58 @@
+// SPDX-License-Identifier: agpl-3.0
+pragma solidity 0.8.10;
+
+import {IERC20} from '@aave/core-v3/contracts/dependencies/openzeppelin/contracts/IERC20.sol';
+import {ICollector} from './interfaces/ICollector.sol';
+import {Ownable} from '@aave/core-v3/contracts/dependencies/openzeppelin/contracts/Ownable.sol';
+
+/**
+ * @title CollectorController
+ * @notice The CollectorController contracts allows the owner
+           of the contract to approve or transfer tokens from
+           the collector proxy contract. This is needed because of
+           the transparent proxy pattern. The admin
+           of the AaveCollector proxy can't be the same as
+           the fundsAdmin address.
+ * @author Aave
+ **/
+contract CollectorController is Ownable {
+  ICollector public immutable COLLECTOR;
+
+  /**
+   * @dev Constructor setups the ownership of the contract and the collector proxy
+   * @param owner The address of the owner of the CollectorController
+   * @param collectorProxy The address of the Collector transparent proxy
+   */
+  constructor(address owner, address collectorProxy) {
+    transferOwnership(owner);
+    COLLECTOR = ICollector(collectorProxy);
+  }
+
+  /**
+   * @dev Transfer an amount of tokens to the recipient.
+   * @param token The address of the asset
+   * @param recipient The address of the entity to transfer the tokens.
+   * @param amount The amount to be transferred.
+   */
+  function approve(
+    IERC20 token,
+    address recipient,
+    uint256 amount
+  ) external onlyOwner {
+    COLLECTOR.approve(token, recipient, amount);
+  }
+
+  /**
+   * @dev Transfer an amount of tokens to the recipient.
+   * @param token The address of the asset
+   * @param recipient The address of the entity to transfer the tokens.
+   * @param amount The amount to be transferred.
+   */
+  function transfer(
+    IERC20 token,
+    address recipient,
+    uint256 amount
+  ) external onlyOwner {
+    COLLECTOR.transfer(token, recipient, amount);
+  }
+}
---
+++ contracts/treasury/interfaces/ICollector.sol
@@ -0,0 +1,54 @@
+// SPDX-License-Identifier: agpl-3.0
+pragma solidity 0.8.10;
+
+import {IERC20} from '@aave/core-v3/contracts/dependencies/openzeppelin/contracts/IERC20.sol';
+
+interface ICollector {
+  /**
+   * @dev Approve an amount of tokens to be pulled by the recipient.
+   * @param token The address of the asset
+   * @param recipient The address of the entity allowed to pull tokens
+   * @param amount The amount allowed to be pulled. If zero it will revoke the approval.
+   */
+  function approve(
+    IERC20 token,
+    address recipient,
+    uint256 amount
+  ) external;
+
+  /**
+   * @dev Transfer an amount of tokens to the recipient.
+   * @param token The address of the asset
+   * @param recipient The address of the entity to transfer the tokens.
+   * @param amount The amount to be transferred.
+   */
+  function transfer(
+    IERC20 token,
+    address recipient,
+    uint256 amount
+  ) external;
+
+  /**
+   * @dev Initialize the transparent proxy with the admin of the Collector
+   * @param reserveController The address of the admin that controls Collector
+   */
+  function initialize(address reserveController) external;
+
+  /**
+   * @dev Retrieve the current funds administrator
+   * @return The address of the funds administrator
+   */
+  function getFundsAdmin() external view returns (address);
+
+  /**
+   * @dev Transfer the ownership of the funds administrator role.
+   * @param admin The address of the new funds administrator
+   */
+  function setFundsAdmin(address admin) external;
+
+  /**
+   * @dev Retrieve the current implementation Revision of the proxy
+   * @return The revision version
+   */
+  function REVISION() external view returns (uint256);
+}

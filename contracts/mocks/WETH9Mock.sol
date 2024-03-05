2ab67c6628a9cbdc6a6f8ee6c827e6e26cbec690
miguelmtzinf <miguelmtz.mail@gmail.com>
2024-03-05 18:15:41 +0100

    fix: Add protection feature in WETH9Mock and bump version

--- contracts/mocks/WETH9Mock.sol
+++ contracts/mocks/WETH9Mock.sol
@@ -5,16 +5,37 @@ import {WETH9} from '@aave/core-v3/contracts/dependencies/weth/WETH9.sol';
 import {Ownable} from '@aave/core-v3/contracts/dependencies/openzeppelin/contracts/Ownable.sol';
 
 contract WETH9Mock is WETH9, Ownable {
+  bool internal _protected;
+
+  /**
+   * @dev Function modifier, if _permissioned is enabled then msg.sender is required to be the owner
+   */
+  modifier onlyOwnerIfProtected() {
+    if (_protected == true) {
+      require(owner() == _msgSender(), 'Ownable: caller is not the owner');
+    }
+    _;
+  }
+
   constructor(string memory mockName, string memory mockSymbol, address owner) {
     name = mockName;
     symbol = mockSymbol;
 
     transferOwnership(owner);
+    _protected = true;
   }
 
-  function mint(address account, uint256 value) public onlyOwner returns (bool) {
+  function mint(address account, uint256 value) public onlyOwnerIfProtected returns (bool) {
     balanceOf[account] += value;
     emit Transfer(address(0), account, value);
     return true;
+  }
+
+  function setProtected(bool state) public onlyOwner {
+    _protected = state;
+  }
+
+  function isProtected() public view returns (bool) {
+    return _protected;
   }
 }
